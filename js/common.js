ef539553079329e80276e17ea82ed84caabb2a51
KapishDima <kapishdima@gmail.com>
2019-05-05 13:18:17 +0300

    Сверстано адаптивное меню

--- js/common.js
+++ js/common.js
@@ -4,6 +4,14 @@ $(document).ready(function () {
     $('.navbar__block-menu-burger-link').on('click', function(e) {
         e.preventDefault;
         $(this).toggleClass('navbar__block-menu-burger-link-active');
+        $('.navbar__hidden').toggleClass('navbar__hidden-active');
+    });
+
+    //Hidden menu close
+    $('.navbar__hidden-wrapper-close-link').on('click', function(e) {
+        e.preventDefault;
+        $('.navbar__hidden').removeClass('navbar__hidden-active');
+        $('.navbar__block-menu-burger-link').removeClass('navbar__block-menu-burger-link-active');
     });
 
 });
